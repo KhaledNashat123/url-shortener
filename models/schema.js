dccae3adca1a0becf71a6b28ea98348010d7efd9
Khaled Nashat <KhaledNashat40@gmail.com>
2024-12-04 03:48:08 +0200

    fixing some error in dynamic view & Add POST route to save URL and alias, render updated list

--- models/schema.js
+++ models/schema.js
@@ -1,26 +1,16 @@
 const mongoose = require('mongoose');
 
 const urlschema = new mongoose.Schema({
-    LongURL: {
+    urlInput: {
         type: String,
         required:true,
     },
-    ShortenedURL: {
+    aliasInput: {
         type: String,
         required:true,
     }
 });
 
 const URL = mongoose.model('URL',urlschema , 'urls'); //note that if we don't name the collection with (urls) it will automatically call it "urls" beacause of lowercase and ploralize od the model name URL 
-
-URL.inputURL = function (LongURL,ShortenedURL){
-    const addedURL = new URL({
-        LongURL: LongURL,
-        ShortenedURL: ShortenedURL,
-    });
-
-    addedURL.save().then(()=> console.log("The new URL added successfully"));
-    
-}
 
 module.exports = URL;
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,48 @@
 const router = require('express').Router();
-const schema = require('../models/schema');
+const URL = require('../models/schema');
 const connection_to_database = require('../check/check');
+const express = require('express');
+
 
 connection_to_database();
 
-router.get('/', async function(req,res,next){
-    const urls = await schema.find();
+router.use(express.urlencoded({ extended: true }));  // For parsing form data
+router.use(express.json());  // For parsing JSON data
+
+router.get('/', async function(req,res){
+    const urls = await URL.find();
     res.render('index',{
         title: 'Express',
         URLs: urls
     }
     );
+})
+
+router.post('/', async (req,res)=>{
+    
+    const {urlInput,aliasInput} = req.body;
+
+    if(!urlInput || !aliasInput) {
+        res.status(400).json({error : "both urls is required"});
+    }
+
+    try {
+        const addedURL = new URL ({
+            urlInput:urlInput,
+            aliasInput: aliasInput,
+        });
+
+        await addedURL.save();
+
+        const urls = await URL.find();
+        res.render('index', {
+            title: 'Express',
+            URLs: urls,
+        });
+    } catch (err) {
+        console.error("Error saving URL:", err);
+        res.status(500).send("Error saving URL");
+    }
 })
 
 module.exports = router;
